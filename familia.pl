bc0e5b2a4f24ca17258532a35058a3f917311ecb
limber miranda <limbermirandacanaviri@gmail.com>
2024-02-27 21:32:49 -0400

    27/02

---
+++ familia.pl
@@ -0,0 +1,33 @@
+esHombre(cristiano).
+esHombre(juan).
+esHombre(batista).
+esHombre(mario).
+esHombre(adrian).
+esHombre(luciano).
+esHombre(alvaro).
+esMujer(cristal).
+esMujer(alejandra).
+esMujer(mariel).
+
+
+
+esHijoOHija(cristiano, juan).
+esHijoOHija(cristiano, batista).
+
+esHijoOHija(mariel, batista).
+esHijoOHija(mariel, cristal).
+
+esHijoOHija(batista, alejandra).
+esHijoOHija(cristal, alejandra).
+esHijoOHija(batista, adrian).
+esHijoOHija(cristal, adrian).
+esHijoOHija(batista, luciano).
+esHijoOHija(cristal, luciano).
+esHijoOHija(mario, alvaro).
+esHijoOHija(alejandra, alvaro).
+
+
+
+
+
+
