04061769991c03afa01f9e0bc4b3d662490af1e3
limber miranda <limbermirandacanaviri@gmail.com>
2024-03-01 18:01:14 -0400

    Identificar Hechos mediante Atomos y Estructuras

---
+++ universidad.pl
@@ -0,0 +1,47 @@
+% ATOMOS
+
+% Profesores
+mario.
+arturica.
+andres.
+
+% Materias
+programacion1.
+redes2.
+inteligenciaArtificial.
+
+% Estudiantes
+carla.
+magenta.
+giovana.
+elena.
+
+% HECHOS
+
+esProfesor(mario).
+esProfesor(arturica).
+esProfesor(andres).
+
+materia(programacion1).
+materia(redes2).
+materia(inteligenciaArtificial).
+
+esEstudiante(carla).
+esEstudiante(magenta).
+esEstudiante(giovana).
+esEstudiante(elena).
+
+ensena(programacion1, mario).
+ensena(redes2, mario).
+ensena(programacion1, arturica).
+ensena(inteligenciaArtificial, arturica).
+ensena(redes2, andres).
+
+pasaClases(redes2, carla).
+pasaClases(inteligenciaArtificial, carla).
+pasaClases(programacion1, magenta).
+pasaClases(redes2, magenta).
+pasaClases(inteligenciaArtificial, magenta).
+pasaClases(redes2, giovana).
+pasaClases(inteligenciaArtificial, giovana).
+pasaClases(inteligenciaArtificial, elena).
