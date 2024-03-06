292c7c96784a04bc72a42e7f819224192e73d28c
limber miranda <limbermirandacanaviri@gmail.com>
2024-03-05 21:27:30 -0400

    Hechos base

---
+++ familiaDioses.pl
@@ -0,0 +1,45 @@
+cronos.
+ares.
+hera.
+poseidon.
+afrodita.
+hefesto.
+hipolita.
+zeus.
+baco.
+atena.
+nera.
+hermes.
+
+
+esHombre(cronos).
+esHombre(ares).
+esHombre(poseidon).
+esHombre(hefesto).
+esHombre(zeus).
+esHombre(baco).
+esHombre(hermes).
+
+esMujer(hera).
+esMujer(afrodita).
+esMujer(hipolita).
+esMujer(atena).
+esMujer(nera).
+
+esDescendienteDirecto(hera, cronos).
+esDescendienteDirecto(afrodita, ares).
+esDescendienteDirecto(hefesto, ares).
+esDescendienteDirecto(afrodita, hera).
+esDescendienteDirecto(hefesto, hera).
+
+
+esDescendienteDirecto(zeus, poseidon).
+esDescendienteDirecto(zeus, afrodita).
+
+esDescendienteDirecto(baco, afrodita).
+esDescendienteDirecto(baco, hefesto).
+esDescendienteDirecto(atena, afrodita).
+esDescendienteDirecto(atena, hefesto).
+esDescendienteDirecto(nera, hipolita).
+esDescendienteDirecto(nera, zeus).
+esDescendienteDirecto(hermes, atena).
