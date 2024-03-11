7b7b3e7750fe2ddff7f431e2699e00258d645d00
limber miranda <limbermirandacanaviri@gmail.com>
2024-03-11 14:17:23 -0400

    Reglas en PROLOG

--- familiaDioses.pl
+++ familiaDioses.pl
@@ -1,3 +1,4 @@
+% ATOMOS
 cronos.
 ares.
 hera.
@@ -11,7 +12,7 @@ atena.
 nera.
 hermes.
 
-
+% HECHOS
 esHombre(cronos).
 esHombre(ares).
 esHombre(poseidon).
@@ -31,11 +32,8 @@ esDescendienteDirecto(afrodita, ares).
 esDescendienteDirecto(hefesto, ares).
 esDescendienteDirecto(afrodita, hera).
 esDescendienteDirecto(hefesto, hera).
-
-
 esDescendienteDirecto(zeus, poseidon).
 esDescendienteDirecto(zeus, afrodita).
-
 esDescendienteDirecto(baco, afrodita).
 esDescendienteDirecto(baco, hefesto).
 esDescendienteDirecto(atena, afrodita).
@@ -43,3 +41,16 @@ esDescendienteDirecto(atena, hefesto).
 esDescendienteDirecto(nera, hipolita).
 esDescendienteDirecto(nera, zeus).
 esDescendienteDirecto(hermes, atena).
+
+% REGLAS
+esPadre(Padre, Hijo) :- esHombre(Padre), esDescendienteDirecto(Hijo, Padre).
+esMadre(Madre, Hijo) :- esMujer(Madre), esDescendienteDirecto(Hijo, Madre).
+
+esHijo(Hijo, Padre) :- esDescendienteDirecto(Hijo, Padre), esHombre(Hijo).
+esHija(Hija, Madre) :- esDescendienteDirecto(Hija, Madre), esMujer(Hija).
+
+esHermano(Hermano1, Hermano2) :- esHombre(Hermano1), esDescendienteDirecto(Hermano1, Padre), esDescendienteDirecto(Hermano2, Padre), Hermano1 \= Hermano2.
+esHermana(Hermana1, Hermana2) :- esMujer(Hermana1), esDescendienteDirecto(Hermana1, Madre), esDescendienteDirecto(Hermana2, Madre), Hermana1 \= Hermana2.
+
+esAbuelo(Abuelo, Nieto) :- esHombre(Abuelo), esDescendienteDirecto(Padre, Abuelo), esDescendienteDirecto(Nieto, Padre).
+esAbuela(Abuela, Nieto) :- esMujer(Abuela), esDescendienteDirecto(Madre, Abuela), esDescendienteDirecto(Nieto, Madre).
